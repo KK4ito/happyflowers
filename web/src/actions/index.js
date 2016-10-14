8bf954933691513bfe8ecf86a8d11f43c4225609
Sacha Schmid <schmid.sacha@gmail.com>
2016-10-14 15:56:33 +0200

    Improve action creators

--- web/src/actions/index.js
+++ web/src/actions/index.js
@@ -1,41 +1,51 @@
 import axios from 'axios'
 
+export const requestFetchSettings = () => ({
+  type: 'FETCH_SETTINGS_REQUEST'
+})
+
+export const succeedFetchSettings = (data) => ({
+  type: 'FETCH_SETTINGS_SUCCESS',
+  data: data
+})
+
+export const failFetchSettings = () => ({
+  type: 'FETCH_SETTINGS_ERROR'
+})
+
 export const fetchSettings = () => (dispatch) => {
-  dispatch({
-    type: 'FETCH_SETTINGS_REQUEST'
-  })
+  dispatch(requestFetchSettings())
 
   return axios.get('http://localhost:5000/settings')
     .then(res => {
-      dispatch({
-        type: 'FETCH_SETTINGS_SUCCESS',
-        data: res.data
-      })
+      dispatch(succeedFetchSettings(res.data))
     })
     .catch(err => {
-      dispatch({
-        type: 'FETCH_SETTINGS_ERROR'
-      })
+      dispatch(failFetchSettings())
     })
-
-
 }
 
+export const requestSubmitSettings = () => ({
+  type: 'SUBMIT_SETTINGS_REQUEST'
+})
+
+export const succeedSubmitSettings = (data) => ({
+  type: 'SUBMIT_SETTINGS_SUCCESS',
+  data: data
+})
+
+export const failSubmitSettings = () => ({
+  type: 'SUBMIT_SETTINGS_ERROR'
+})
+
 export const submitSettings = (data) => (dispatch) => {
-  dispatch({
-    type: 'SUBMIT_SETTINGS_REQUEST'
-  })
+  dispatch(requestSubmitSettings())
 
   return axios.put('http://localhost:5000/settings', data)
     .then(res => {
-      dispatch({
-        type: 'SUBMIT_SETTINGS_SUCCESS',
-        data: res.data
-      })
+      dispatch(succeedSubmitSettings(res.data))
     })
     .catch(err => {
-      dispatch({
-        type: 'SUBMIT_SETTINGS_ERROR'
-      })
+      dispatch(failSubmitSettings())
     })
 }
