bbf2d67b1488b5a735f0e4fbbdce3c0c3adbd5a4
Sacha Schmid <schmid.sacha@gmail.com>
2016-11-21 12:54:55 +0100

    Allow display of livestream in fullscreen

--- web/src/containers/Stream.js
+++ web/src/containers/Stream.js
@@ -9,11 +9,28 @@ import './Stream.css'
  *
  * @return {string} - HTML markup for the component.
  */
-const Stream = ({ name }) => (
-  <Widget title="Livestream">
-    <img className="live-stream" src={`http://${window.location.hostname}:8081`} alt={`Livestream of ${name}.`} />
-  </Widget>
-)
+class Stream extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      fullscreen: false
+    }
+  }
+
+  render() {
+    const { name } = this.props
+
+    return (
+      <Widget title="Livestream">
+        <div onClick={() => this.setState({ fullscreen: false })} className={`full-screen ${this.state.fullscreen ? 'is-visible' : ''}`}>
+          <img className="live-stream" src={`http://${window.location.hostname}:8081`} alt={`Livestream of ${name}.`} />
+        </div>
+        <img onClick={() => this.setState({ fullscreen: true })} className="live-stream" src={`http://${window.location.hostname}:8081`} alt={`Livestream of ${name}.`} />
+      </Widget>
+    )
+  }
+}
 
 const mapStateToProps = state => ({
   name: state.settings.data.get('name')
