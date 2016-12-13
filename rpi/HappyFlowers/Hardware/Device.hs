6d13f2ae815573fab1c5326cf1d2f07a3d150b7e
Sacha Schmid <schmid.sacha@gmail.com>
2016-12-13 16:25:56 +0100

    Purify functions

--- rpi/HappyFlowers/Hardware/Device.hs
+++ rpi/HappyFlowers/Hardware/Device.hs
@@ -15,23 +15,24 @@ pumpDelay = 5000000
 
 -- | reads data about moisture from the chirp sensor.
 readMoisture :: IO Int
-readMoisture = do
-    val <- I2C.read 0
+readMoisture = fmap convertMoisture (I2C.read 0)
 
+-- | converts I2C moisture to a relative value between 0 and 100.
+convertMoisture :: Int -> Int
+convertMoisture val = do
     let numeral = fromIntegral (val - 100) :: Rational
     let relative = round (numeral / 700.0 * 100.0) :: Int
-
-    case relative of
-        _ | relative < 0   -> return 0
-          | relative > 100 -> return 100
-          | otherwise      -> return relative
+    min 100 $ max 0 val
 
 -- | reads data about temperature from the chirp sensor.
 readTemperature :: IO Int
-readTemperature = do
-    val <- I2C.read 5
+readTemperature = fmap convertTemperature (I2C.read 5)
+
+-- | converts I2C temperature to degrees celcius.
+convertTemperature :: Int -> Int
+convertTemperature val = do
     let numeral = fromIntegral val :: Rational
-    return (round (numeral / 10.0) :: Int)
+    round (numeral / 10.0) :: Int
 
 -- | triggers the USB water pump.
 triggerPump :: IO ()
--- rpi/HappyFlowers/Hardware/Mock.hs
+++ rpi/HappyFlowers/Hardware/Mock.hs
@@ -12,12 +12,22 @@ pumpDelay = 5000000
 
 -- | reads data about moisture from the chirp sensor.
 readMoisture :: IO Int
-readMoisture = putStrLn "sensor on" >> putStrLn "sensor off" >> return 80
+readMoisture = do
+    putStrLn "sensor on"
+    putStrLn "sensor off"
+    return 80
 
 -- | reads data about temperature from the chirp sensor.
 readTemperature :: IO Int
-readTemperature = putStrLn "sensor on" >> putStrLn "sensor off" >> return 25
+readTemperature = do
+    putStrLn "sensor on"
+    putStrLn "sensor off"
+    return 25
 
 -- | triggers the USB water pump.
 triggerPump :: IO ()
-triggerPump = putStrLn "pump on" >> threadDelay pumpDelay >> putStrLn "pump off" >> threadDelay pumpDelay
+triggerPump = do
+    putStrLn "pump on"
+    threadDelay pumpDelay
+    putStrLn "pump off"
+    threadDelay pumpDelay
